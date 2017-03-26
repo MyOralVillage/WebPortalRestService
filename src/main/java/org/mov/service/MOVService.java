996e1eb7706e6aff604974a3239cd9dc1de70931
LeontiBrechko <leontibrechko@gmail.com>
2017-03-26 15:17:56 -0400

    updated application service

--- src/main/java/org/mov/service/MOVService.java
+++ src/main/java/org/mov/service/MOVService.java
@@ -1,18 +1,44 @@
 package org.mov.service;
 
+import org.mov.model.Country;
 import org.mov.model.Document;
+import org.mov.model.Theme;
 import org.mov.model.User;
+
+import java.util.Collection;
 
 public interface MOVService {
     void saveUser(User user);
 
-    void saveDocument(Document document);
-
     void removeUser(User user);
-
-    void removeDocument(Document document);
 
     User findUserById(Long id);
 
+    User findUserByEmail(String email);
+
+    void saveDocument(Document document);
+
+    void removeDocument(Document document);
+
     Document findDocumentById(Long id);
+
+    void saveTheme(Theme theme);
+
+    void removeTheme(Theme theme);
+
+    Theme findThemeById(Long id);
+
+    Theme findThemeByName(String name);
+
+    Collection<Theme> findAllThemes();
+
+    void saveCountry(Country country);
+
+    void removeCountry(Country country);
+
+    Country findCountryById(Long id);
+
+    Country findCountryByName(String name);
+
+    Collection<Country> findAllCountries();
 }
--- src/main/java/org/mov/service/MOVServiceImpl.java
+++ src/main/java/org/mov/service/MOVServiceImpl.java
@@ -1,57 +1,121 @@
 package org.mov.service;
 
+import org.mov.model.Country;
 import org.mov.model.Document;
+import org.mov.model.Theme;
 import org.mov.model.User;
+import org.mov.repository.CountryRepository;
 import org.mov.repository.DocumentRepository;
+import org.mov.repository.ThemeRepository;
 import org.mov.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
+
 @Service
+@Transactional
 public class MOVServiceImpl implements MOVService {
     private DocumentRepository documentRepository;
     private UserRepository userRepository;
+    private ThemeRepository themeRepository;
+    private CountryRepository countryRepository;
 
     @Autowired
-    public MOVServiceImpl(UserRepository userRepository, DocumentRepository documentRepository) {
+    public MOVServiceImpl(UserRepository userRepository,
+                          DocumentRepository documentRepository,
+                          ThemeRepository themeRepository,
+                          CountryRepository countryRepository) {
         this.userRepository = userRepository;
         this.documentRepository = documentRepository;
+        this.themeRepository = themeRepository;
+        this.countryRepository = countryRepository;
     }
 
     @Override
-    @Transactional
     public void saveUser(User user) {
         userRepository.saveUser(user);
     }
 
     @Override
-    @Transactional
-    public void saveDocument(Document document) {
-        documentRepository.saveDocument(document);
-    }
-
-    @Override
-    @Transactional
     public void removeUser(User user) {
         userRepository.removeUser(user);
     }
 
     @Override
-    @Transactional
+    public User findUserById(Long id) {
+        return userRepository.findUserById(id);
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        return userRepository.findUserByEmail(email);
+    }
+
+    @Override
+    public void saveDocument(Document document) {
+        documentRepository.saveDocument(document);
+    }
+
+    @Override
     public void removeDocument(Document document) {
         documentRepository.removeDocument(document);
     }
 
     @Override
     @Transactional(readOnly = true)
-    public User findUserById(Long id) {
-        return userRepository.findUserById(id);
+    public Document findDocumentById(Long id) {
+        return documentRepository.findDocumentById(id);
     }
 
     @Override
-    @Transactional(readOnly = true)
-    public Document findDocumentById(Long id) {
-        return documentRepository.findDocumentById(id);
+    public void saveTheme(Theme theme) {
+        themeRepository.saveTheme(theme);
+    }
+
+    @Override
+    public void removeTheme(Theme theme) {
+        themeRepository.removeTheme(theme);
+    }
+
+    @Override
+    public Theme findThemeById(Long id) {
+        return themeRepository.findThemeById(id);
+    }
+
+    @Override
+    public Theme findThemeByName(String name) {
+        return themeRepository.findThemeByName(name);
+    }
+
+    @Override
+    public Collection<Theme> findAllThemes() {
+        return themeRepository.findAllThemes();
+    }
+
+    @Override
+    public void saveCountry(Country country) {
+        countryRepository.saveCountry(country);
+    }
+
+    @Override
+    public void removeCountry(Country country) {
+        countryRepository.removeCountry(country);
+    }
+
+    @Override
+    public Country findCountryById(Long id) {
+        return countryRepository.findCountryById(id);
+    }
+
+    @Override
+    public Country findCountryByName(String name) {
+        return countryRepository.findCountryByName(name);
+    }
+
+    @Override
+    public Collection<Country> findAllCountries() {
+        return countryRepository.findAllCountries();
     }
 }
